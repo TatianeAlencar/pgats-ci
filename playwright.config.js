f2f42ef1a77fc69eea458ab08ffac1f0c99e6053
talencar <tatiane_alencaryahoo.com.brgit config --global user.email tatiane_alencar@yahoo.com.brgit config --global user.email tatiane_alencaryahoo.com.br>
2026-05-31 23:48:58 +0100

    "adiciona relatórios allure"

--- playwright.config.js
+++ playwright.config.js
@@ -21,7 +21,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html', { open: false }], ['junit', { outputFile: `results.xml` }]],
+  reporter: [['html', { open: false }], ['junit', { outputFile: `results.xml` }], ['allure-playwright']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -29,7 +29,8 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    screenshot: 'only-on-failure'
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure'
   },
 
   /* Configure projects for major browsers */
@@ -66,4 +67,5 @@ export default defineConfig({
   //   url: 'http://127.0.0.1:3000',
   //   reuseExistingServer: !process.env.CI,
   // },
+  
 });
